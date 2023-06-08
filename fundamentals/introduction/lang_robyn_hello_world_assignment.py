50fc99d29881ed01878c3e889ce4472020d27baa
RobynTraciLang <jetblackkinks@gmail.com>
2023-06-08 07:14:38 -0500

    Today's updates from the Learn platform.

--- fundamentals/introduction/lang_robyn_hello_world_assignment.py
+++ fundamentals/introduction/lang_robyn_hello_world_assignment.py
@@ -37,3 +37,27 @@ copy_of_words.append("dojo") # only appends to the copy
 print(copy_of_words) # prints ['start', 'going', 'till', 'the', 'end', 'dojo']
 print(words) # prints ['start', 'going', 'till', 'the', 'end']
 
+some_nums = [44,56,2,3,12,19,6]
+print("Get started by writing your own code!")
+
+# Some optional challenges to assess and refine your understanding:
+
+# Print the length of the list.
+print(len(some_nums))
+
+# Use another python built-in function and print the result.
+print(max(some_nums))
+print(min(some_nums))
+
+# Remove an item from the list. Remember to verify that it was removed.
+some_nums.pop()
+print(some_nums)
+
+# Utilize a method from the documentation and print the result.
+some_nums.reverse()
+print(some_nums)
+
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
