f09d088f6c099fbb2f22afadccad3afb423ee54b
RobynTraciLang <jetblackkinks@gmail.com>
2023-07-14 14:11:52 -0500

    finished understanding routes assignment (practice)

--- flask/fundamentals/lang_robyn_understanding_routes/server.py
+++ flask/fundamentals/lang_robyn_understanding_routes/server.py
@@ -7,7 +7,7 @@
 # Create one url pattern and function that can handle the following examples (HINT: path variables are by default passed as strings. How might you handle a number?):
 # localhost:5000/repeat/35/hello - have it say "hello" 35 times DONE
 # localhost:5000/repeat/80/bye - have it say "bye" 80 times DONE
-# localhost:5000/repeat/99/dogs - have it say "dogs" 99 times
+# localhost:5000/repeat/99/dogs - have it say "dogs" 99 times DONE
 
 from flask import Flask
 app = Flask(__name__)
@@ -28,26 +28,26 @@ def say_flask():
 def say_john(name):
     return f"Hi {name.capitalize()}!"
 
+# @app.route('/repeat/<int:num>/<string:word>')
+# def repeat_num_word(num, word):
+#     return (word + " ") * num
+# original way I did it
+
 @app.route('/repeat/<int:num>/<string:word>')
-def repeat_num_hello(num, word):
-    return (word + " ") * num
+def repeat_num_word(num, word):
+    output = ""
 
-# @app.route('/repeat/<int:num>/hello')
-# def repeat_num_hello(num):
-#     return 'hello ' * num
+    for i in range(num):
+        output += f"<p>{word}</p>"
 
-# @app.route('/repeat/<int:num>/<string:bye>')
-# def repeat_num_bye(num):
-#     return 'bye ' * num
+    return output
+# tried this after viewing the solution video
 
-# @app.route('/repeat/<int:num>/dogs')
-# def repeat_num_dogs(num):
-#     return 'dogs ' * num
-
-# @app.route('/')
-#     return "Sorry! No response. Try again."
+@app.route('/<path:invalid_route>')
+def handle_invalid_route(invalid_route):
+    return "Sorry! No response. Try again."
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False) #usually True, but has to be set to False for the invalid route app to be recognized.
 
 
